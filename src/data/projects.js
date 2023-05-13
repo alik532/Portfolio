9371b06c4dd4ea23f4f8f6f766502ed395a2d9ed
Alibek <alibeknurtuleu@gmail.com>
2023-05-13 10:23:18 +0600

    added travel app

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,7 @@ import reduxLogo from '../components/UI/reduxLogo.svg'
 import apiLogo from '../components/UI/apiLogo.svg'
 import firebaseLogo from '../components/UI/firebaseLogo.svg'
 import openseaImg from '../images/opensea.png'
+import travel from '../images/travel.png'
 
 export const projectsData = [
     {title: "React E-Commerce app", stack: [{name: 'react', logo: reactLogo}, {name: 'redux', logo: reduxLogo}], description: "React Redux E-commerce project with ready-made functionality for adding a product and a shopping cart", 
@@ -15,5 +16,7 @@ export const projectsData = [
     {title: "Spotify App", stack: [{name: 'react', logo: reactLogo}, {name: 'redux', logo: reduxLogo}, {name: 'rest API', logo: apiLogo}], description: "The spotify clone App using React, Redux-Toolkit, axios requests from spotify API and React best practices.", 
     githubLink: "https://github.com/alik532/SpotifyClone", appLink: "https://celebrated-brioche-b46c55.netlify.app/", img: spotifyImg},
     {title: "NFT-Market", stack: [{name: 'react', logo: reactLogo}, {name: 'redux', logo: reduxLogo}, {name: 'rest API', logo: apiLogo}, {name: "firebase", logo: firebaseLogo}], description: "The nft marketplace app using react redux with firebase authorization, also cart, fetching data, pagination functionality, data sorting functionality.", 
-    githubLink: "https://github.com/alik532/NFT-Marketplace", appLink: "https://effortless-heliotrope-6a592a.netlify.app/", img: openseaImg}
+    githubLink: "https://github.com/alik532/NFT-Marketplace", appLink: "https://effortless-heliotrope-6a592a.netlify.app/", img: openseaImg},
+    {title: "Travel App", stack: [{name: 'react', logo: reactLogo}], description: "The travel app using ReactJS and responsive layout.", 
+    githubLink: "https://github.com/alik532/travel_react_app", appLink: "https://sensational-mandazi-84e8e5.netlify.app", img: travel},
 ]
